08121addc13f40028d29610fea52faa7a0fb1b3d
IdaMarcherJensen <54858@student.eadania.dk>
2022-09-16 10:58:51 +0200

    update minesweeper

--- BasisProgrammering - Projekt uge/BasisProgrammering - Projekt uge/Program.cs
+++ BasisProgrammering - Projekt uge/BasisProgrammering - Projekt uge/Program.cs
@@ -13,11 +13,42 @@ namespace BasisProgrammering___Projekt_uge
 
             // TODO: make a menu for the games 
 
-            
+            MineSweeper(); 
+
+            Console.ReadLine(); 
         }
 
         static void MineSweeper()
         {
+            // promt user for board size 
+            Console.WriteLine("How big should the board be?");
+            int boardHeight = Convert.ToInt32(Console.ReadLine());
+            int boardWidth = Convert.ToInt32(Console.ReadLine()); 
+            
+            // set board size with user input 
+            int[,] board = new int[boardHeight,boardWidth];
+
+            // promt user for how many bombs
+            Console.WriteLine("How many bombs? \n\tMin: 0 \n\tMax: {0}", boardHeight*boardWidth);
+            int numberOfBombs = Convert.ToInt32(Console.ReadLine());
+
+            DrawBoardMS(board, numberOfBombs); 
+        }
+
+        static void DrawBoardMS(int[,] board, int numberOfBombs)
+        {
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+
+                }
+
+            }
+
+
+
 
         }
 
