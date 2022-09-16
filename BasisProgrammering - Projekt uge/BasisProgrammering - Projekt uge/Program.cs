4926f44c457dc93a5d733c9768863cb93590ee49
IdaMarcherJensen <54858@student.eadania.dk>
2022-09-16 14:06:13 +0200

    update count bombs

--- BasisProgrammering - Projekt uge/BasisProgrammering - Projekt uge/Program.cs
+++ BasisProgrammering - Projekt uge/BasisProgrammering - Projekt uge/Program.cs
@@ -46,7 +46,6 @@ namespace BasisProgrammering___Projekt_uge
 
         static void MineSweeper()
         {
-
             // promt user for board size 
             Console.WriteLine("How big should the board be?");
             int boardHeight = Convert.ToInt32(Console.ReadLine());
@@ -59,25 +58,52 @@ namespace BasisProgrammering___Projekt_uge
             Console.WriteLine("How many bombs? \n\tMin: 0 \n\tMax: {0}", boardHeight*boardWidth);
             int numberOfBombs = Convert.ToInt32(Console.ReadLine());
 
-            SetBombsMS(board, numberOfBombs); 
+            // set the bombs on the board using function 
+            SetBombsMS(board, numberOfBombs);
 
+            BombsAroundMS(board);
+
+            DrawBoardMS(board); 
 
         }
 
+        static void DrawBoardMS(int[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    Console.Write(board[i,j]+" "); 
+                }
+                Console.WriteLine(); 
+            }
+        }
+
+        /// <summary>
+        /// MineSweeper 
+        /// function that sets the bombs on the board 
+        /// </summary>
+        /// <param name="board">predefined board from multidimentional array</param>
+        /// <param name="numberOfBombs">predefined number of bombs that has to be placed on the board</param>
         static void SetBombsMS(int[,] board, int numberOfBombs)
         {
-            // set the bombs on the board 
+            // set bomb randomly on board 
+            Random random = new Random();
+
+            // set all the bombs on the board 
             while (numberOfBombs > 0)
             {
                 for (int i = 0; i < board.GetLength(0); i++)
                 {
                     for (int j = 0; j < board.GetLength(1); j++)
                     {
-                        // set bomb randomly on board 
-                        Random random = new Random();
-                        if (board[i,j] == 0 && random.Next(1) == 1)
+                        // only set bomb on position if there is no bomb present 
+                        // only set if allowed by random 
+                        if (board[i,j] == 0 && random.Next(5) == 1)
                         {
+                            // mark the bomb on the board 
                             board[i, j] = 35; // ascii: 35 = # 
+                            // decrease amount of bombs 
                             numberOfBombs--; 
                         }
                     }
@@ -85,7 +111,130 @@ namespace BasisProgrammering___Projekt_uge
             }
         }
 
+        /// <summary>
+        /// MineSweeper
+        /// function that checks how many bombs are around each spot 
+        /// </summary>
+        /// <param name="board"></param>
+        static void BombsAroundMS(int[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    // save the position 
+                    int boardPosI = i;
+                    int boardPosJ = j;
 
+                    // corner cases 
+                    if (i == 0 && j == 0)
+                    {
+                        // if position at top left corner of board 
+                        // positions to check 
+                        int[] checkPositionXtl = new int[2] { 0, 1 };
+                        int[] checkPositionYtl = new int[2] { 0, 1 };
+                        // set number on board, of how many bombs are nearby, with function 
+                        Console.WriteLine("Hej");
+                        board[i, j] = CountBombs(boardPosI, boardPosJ, board, checkPositionXtl, checkPositionYtl);
+                    }
+                    else if (i == 0 && j == board.GetLength(1) - 1)
+                    {
+                        // if position at top right corner of board 
+                        // positions to check 
+                        int[] checkPositionXtr = new int[2] { 0, 1 };
+                        int[] checkPositionYtr = new int[2] { -1, 0 };
+                        // set number on board, of how many bombs are nearby, with function 
+                        board[i, j] = CountBombs(boardPosI, boardPosJ, board, checkPositionXtr, checkPositionYtr);
+                    }
+                    else if (i == board.GetLength(0) - 1 && j == 0)
+                    {
+                        // if position at bottom left corner 
+                        // positions to check 
+                        int[] checkPositionXbl = new int[2] { -1, 0 };
+                        int[] checkPositionYbl = new int[2] { 0, 1 };
+                        // set number on board, of how many bombs are nearby, with function 
+                        board[i, j] = CountBombs(boardPosI, boardPosJ, board, checkPositionXbl, checkPositionYbl);
+                    }
+                    else if (i == board.GetLength(0) - 1 && j == board.GetLength(1) - 1)
+                    {
+                        // if position at bottom right corner 
+                        // positions to check 
+                        int[] checkPositionXbr = new int[2] { -1, 0 };
+                        int[] checkPositionYbr = new int[2] { -1, 0 };
+                        // set number on board, of how many bombs are nearby, with function 
+                        board[i, j] = CountBombs(boardPosI, boardPosJ, board, checkPositionXbr, checkPositionYbr);
+                    }
+                    // edge cases 
+                    else if (i == 0)
+                    {
+                        // if position at top edge of board 
+                        // positions to check 
+                        int[] checkPositionXtop = new int[2] { 0, 1 };
+                        int[] checkPositionYtop = new int[3] { -1, 0, 1 };
+                        // set number on board, of how many bombs are nearby, with function 
+                        board[i, j] = CountBombs(boardPosI, boardPosJ, board, checkPositionXtop, checkPositionYtop);
+                    }
+                    else if (i == board.GetLength(0)-1)
+                    {
+                        // if position at bottom edge of board
+                        // positions to check 
+                        int[] checkPositionXbottom = new int[2] { -1, 0 };
+                        int[] checkPositionYbottom = new int[3] { -1, 0, 1 };
+                        // set number on board, of how many bombs are nearby, with function 
+                        board[i, j] = CountBombs(boardPosI, boardPosJ, board, checkPositionXbottom, checkPositionYbottom);
+                    }
+                    else if(j == 0)
+                    {
+                        // if position at left edge of board
+                        // positions to check 
+                        int[] checkPositionXleft = new int[3] { -1, 0, 1 };
+                        int[] checkPositionYleft = new int[2] { 0, 1 };
+                        // set number on board, of how many bombs are nearby, with function 
+                        board[i, j] = CountBombs(boardPosI, boardPosJ, board, checkPositionXleft, checkPositionYleft);
+                    }
+                    else if(j == board.GetLength(1) - 1)
+                    {
+                        // if position at right edge of board
+                        // positions to check 
+                        int[] checkPositionXright = new int[3] { -1, 0, 1 };
+                        int[] checkPositionYright = new int[2] { -1, 0 };
+                        // set number on board, of how many bombs are nearby, with function 
+                        board[i, j] = CountBombs(boardPosI, boardPosJ, board, checkPositionXright, checkPositionYright);
+                    }
+                    
+                    // cases everywhere on the board 
+                    else
+                    {
+                        // positions to check 
+                        int[] checkPositionX = new int[3] { -1, 0, 1 };
+                        int[] checkPositionY = new int[3] { -1, 0, 1 };
+                        // set number on board, of how many bombs are nearby, with function 
+                        board[i,j] = CountBombs(boardPosI, boardPosJ, board, checkPositionX, checkPositionY); 
+                    }
+                }
+            }
+        }
+        static int CountBombs(int x, int y, int[,] board, int[] positionX, int[] positionY)
+        {
+            // set initial bomb count to 0 
+            int bombCount = 0; 
+
+            for (int i = 0; i < positionX.Length; i++)
+            {
+                for (int j = 0; j < positionY.Length; j++)
+                {
+                    // if there is a bomb on the board at position i,j 
+                    if (board[x + positionX[i], y + positionY[j]] == 35)
+                    {
+                        // increment bomb count 
+                        bombCount++; 
+                    }
+                }
+            }
+
+            // return the total amount of bombs nearby position 
+            return bombCount; 
+        }
 
 
 
